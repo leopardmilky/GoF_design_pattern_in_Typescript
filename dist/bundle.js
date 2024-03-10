999173c7f2cc0894b4f085ca54d43c8230d67459
leopardmilky <matthew.lee0619@gmail.com>
2024-03-11 01:05:02 +0900

    iterator

---
+++ dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+console.log("디자인 패턴 공부합시다.");
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsT0FBTyxDQUFDLEdBQUcsQ0FBQyxlQUFlLENBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90eXBlc2NyaXB0X2dvZl9kZXNpZ25fcGF0dGVybi8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZyhcIuuUlOyekOyduCDtjKjthLQg6rO167aA7ZWp7Iuc64ukLlwiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
---
+++ src/iterator/ArrayIterator.ts
@@ -0,0 +1,23 @@
+import Array from "./Array";
+import Iterator from "./Iterator";
+import Item from "./Item";
+
+class ArrayIterator implements Iterator<Item> {
+    private index: number
+
+    constructor(private array: Array) {
+        this.index = -1
+    }
+
+    next(): boolean {
+        this.index++
+        return this.index < this.array.count
+    }
+
+    current(): Item {
+        return this.array.getItem(this.index)
+    }
+}
+
+export default ArrayIterator
+
